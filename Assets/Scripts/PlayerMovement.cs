20b8f866b1390c1ff50fc28c0e5b1a1cce943ac6
Maxim Kravchenko <max.kravchenko.dev@gmail.com>
2023-04-29 13:05:06 +0300

    Screen Wraparound

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -22,6 +22,22 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
+        ProcessInput();
+
+        KeepPlayerOnScreen();
+    }
+
+    void FixedUpdate() 
+    {
+        if(movementDirection == Vector3.zero) { return; }
+
+        rb.AddForce(movementDirection * forceMagnitude * Time.deltaTime, ForceMode.Force);
+
+        rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxVelocity);
+    }
+
+    void ProcessInput()
+    {
         if(Touchscreen.current.primaryTouch.press.isPressed)
         {
             Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
@@ -38,12 +54,29 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    void FixedUpdate() 
+    private void KeepPlayerOnScreen()
     {
-        if(movementDirection == Vector3.zero) { return; }
+        Vector3 newPosition = transform.position;
+        Vector3 viewportPosition = mainCamera.WorldToViewportPoint(transform.position);
 
-        rb.AddForce(movementDirection * forceMagnitude * Time.deltaTime, ForceMode.Force);
+        if (viewportPosition.x > 1)
+        {
+            newPosition.x = -newPosition.x + 0.1f;
+        }
+        else if (viewportPosition.x < 0)
+        {
+            newPosition.x = -newPosition.x - 0.1f;
+        }
 
-        rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxVelocity);
+        if (viewportPosition.y > 1)
+        {
+            newPosition.y = -newPosition.y + 0.1f;
+        }
+        else if (viewportPosition.y < 0)
+        {
+            newPosition.y = -newPosition.y - 0.1f;
+        }
+
+        transform.position = newPosition;
     }
 }
