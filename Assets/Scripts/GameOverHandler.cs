bcf1374e9815d9d5c36af8418dcaff6635f57c76
Maxim Kravchenko <max.kravchenko.dev@gmail.com>
2023-04-30 18:56:13 +0300

    Score System

--- Assets/Scripts/GameOverHandler.cs
+++ Assets/Scripts/GameOverHandler.cs
@@ -2,15 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class GameOverHandler : MonoBehaviour
 {
+    [SerializeField] private TMP_Text gameOverText;
+    [SerializeField] private ScoreSystem scoreSystem;
     [SerializeField] private GameObject gameOverDisplay;
     [SerializeField] private AsteroidSpawner asteroidSpawner;
     
     public void EndGame()
     {
         asteroidSpawner.enabled = false;
+
+        scoreSystem.EndTimer();
+        int finalScore = scoreSystem.GetScore();
+        gameOverText.text = $"Your Score: {finalScore}";
 
         gameOverDisplay.gameObject.SetActive(true);
     }
---
+++ Assets/Scripts/ScoreSystem.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreSystem : MonoBehaviour
+{
+    [SerializeField] private TMP_Text scoreText;
+    [SerializeField] private float scoreMultiplier;
+
+    private bool shouldCount = true;
+    private float score;
+    
+    void Update()
+    {
+        if(!shouldCount) { return; }
+        
+        score += Time.deltaTime *  scoreMultiplier;
+        
+        scoreText.text = Mathf.FloorToInt(score).ToString();
+    }
+
+    public int GetScore()
+    {
+        return Mathf.FloorToInt(score);
+    }
+
+    public void EndTimer()
+    {
+        shouldCount = false;
+
+        scoreText.text = string.Empty;
+    }
+}
